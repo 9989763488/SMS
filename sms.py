2eb96fd69d1ccda04c71f6d9764ab14c4e214814
tejaswini <saitejaswini7537@gmail.com>
2018-01-07 19:32:48 +0530

    second commit

--- sms.py
+++ sms.py
@@ -3,24 +3,24 @@ import pyowm
 import time
 
 while(True):
-        owm=pyowm.OWM('7a6c2f736e3fe03a91def1a486e2b49a')
+        owm=pyowm.OWM('--Weather report key---')
         observation=owm.weather_at_place('Delhi,IN')
         w=observation.get_weather()
         humidity=w.get_humidity()
         
         print(humidity)
         
-        acc_sid="ACf25181487f9c996adb6e3b146d157ad5"
-        auth_tok="bdb257515273319afa145bf0d24a9b3d"
+        acc_sid="--account sid-----"
+        auth_tok="---authentication token----"
         client=Client(acc_sid,auth_tok)
         if(humidity>70):
             message=client.messages.create(body="Humidity exceeds set level",
-                                           to="+919618261525",
-                                           from_=" +13394997596")
+                                           to="+91--receiver number",
+                                           from_=" +twilio number")
             print(message.sid)
         else:
             message=client.messages.create(body="humidity is below set level",
-                                           to="+919618261525",
-                                           from_=" +13394997596")
+                                           to="+91 ---receiver number",
+                                           from_=" +twilio number")
             print(message.sid)
         time.sleep(2)
